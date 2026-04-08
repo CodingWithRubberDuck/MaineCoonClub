2bd41364fe397c12a7bd7588782683c14925c30d
augus <august.soeltoft@gmail.com>
2026-04-08 10:54:21 +0200

    Initial Commit

---
+++ src/main/java/com/RaceKatteKlubben/MaineCoonClub/MaineCoonClubApplication.java
@@ -0,0 +1,13 @@
+package com.RaceKatteKlubben.MaineCoonClub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaineCoonClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaineCoonClubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RaceKatteKlubben/MaineCoonClub/MaineCoonClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RaceKatteKlubben.MaineCoonClub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaineCoonClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
