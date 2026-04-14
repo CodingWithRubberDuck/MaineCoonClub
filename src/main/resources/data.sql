b9dbf120edddc76b46376dc12e63d64595ea078c
augus <august.soeltoft@gmail.com>
2026-04-14 22:34:57 +0200

    Tilføjede bare en profil til i startup data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,8 @@
 INSERT INTO member (name, email, password_hash)
-VALUES ('FRANK', 'Frank.Castle@gmail.com', '$2a$10$BQshntA.zOjdXoGd5/5z8.zFtKvW53jpD1abUgL6itiVQbkzkkkda');
+VALUES ('Frank', 'Frank.Castle@gmail.com', '$2a$10$BQshntA.zOjdXoGd5/5z8.zFtKvW53jpD1abUgL6itiVQbkzkkkda');
 INSERT INTO member (name, email, password_hash)
 VALUES ('Bob', 'bo@gmail.com', '$2a$10$DD6mBCpPjFz16IgekBU91.y8otMHyICyDB2l4yfgwUSKiIfwI3ZDS');
 INSERT INTO member (name, email, password_hash)
 VALUES ('Steve', 'SteveIsAFurry@gmail.com', '$2a$10$HEYSXamb.H2Vn13U2XZ8o.v70dEIoXWoXHqiGcgjebEXgLefYtTJu');
+INSERT INTO member (name, email, password_hash)
+VALUES ('Mikkel', 'mikkel@gmail.com', '$2a$10$cQAHYqBHz7JoMR3HYhke0u3uXNJXTrUJSar0Eib7rgdz7z7AKjJcW')
